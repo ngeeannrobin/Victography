bef861087d0a2275114c5b86434a9ebf446ecc5f
Zheng Nan Wesley Qua <s10179874@connect.np.edu.sg>
2018-07-25 21:13:37 +0800

    changed code to be easier to understand Added teh firebase functions

--- app/src/main/java/sg/edu/np/connect/s10179209/victography/PictureDBAdapter.java
+++ app/src/main/java/sg/edu/np/connect/s10179209/victography/PictureDBAdapter.java
@@ -8,11 +8,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.sql.Blob;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import com.google.firebase.database.DataSnapshot;
@@ -21,83 +18,84 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import static sg.edu.np.connect.s10179209.victography.PictureDBAdapter.myDbHelper.CREATE_IMAGE_TABLE;
+import static sg.edu.np.connect.s10179209.victography.PictureDBAdapter.myDbHelper.CREATE_LIKE_TABLE;
+import static sg.edu.np.connect.s10179209.victography.PictureDBAdapter.myDbHelper.CREATE_MEMBER_TABLE;
+import static sg.edu.np.connect.s10179209.victography.PictureDBAdapter.myDbHelper.DROP_IMAGE_TABLE;
+import static sg.edu.np.connect.s10179209.victography.PictureDBAdapter.myDbHelper.DROP_LIKE_TABLE;
+import static sg.edu.np.connect.s10179209.victography.PictureDBAdapter.myDbHelper.DROP_MEMBER_TABLE;
+
 public class PictureDBAdapter extends AppCompatActivity {
     myDbHelper myhelper;
     public PictureDBAdapter(Context context)
     {
         myhelper = new myDbHelper(context);
     }
-    static class myDbHelper extends SQLiteOpenHelper
-    {
+    static class myDbHelper extends SQLiteOpenHelper {
         //myDbHelper attributes
-        private Context context;
+        public Context context;
+        public static final String DATABASE_NAME = "myDatabase";    // Database Name
+        public static final int DATABASE_Version = 1;   // Database Version
         //Defining the Member table variables
-        private static final String DATABASE_NAME = "myDatabase";    // Database Name
-        private static final int DATABASE_Version = 1;   // Database Version
-        private static final String MEMBER_TABLE = "Member";   // Table Name// Column I (Primary Key)
-        private static final String NAME = "username";    //Column II
-        private static final String PASSWORD= "password";    // Column III
+        public static final String MEMBER_TABLE = "Member";   // Table Name// Column I (Primary Key)
+        public static final String NAME = "username";    //Column II
         //Command to create Member table
-        private static final String CREATE_MEMBER_TABLE =
-                "CREATE TABLE "+MEMBER_TABLE
+        public static final String CREATE_MEMBER_TABLE =
+                "CREATE TABLE IF NOT EXISTS " + MEMBER_TABLE
                         + " ("
-                        + NAME+" VARCHAR(255) PRIMARY KEY ,"
-                        + PASSWORD+" VARCHAR(225)"
+                        + NAME + " VARCHAR(255) PRIMARY KEY "
                         + ");";
         //Drop table for Member
-        private static final String DROP_MEMBER_TABLE ="DROP TABLE IF EXISTS "+MEMBER_TABLE;
+        public static final String DROP_MEMBER_TABLE = "DROP TABLE IF EXISTS " + MEMBER_TABLE;
 
         //Defining the Image table variables
-        private static final String IMAGE_TABLE = "Images";   // Table Name
-        private static final String IMAGE_ID="imageID";     // Column I (Primary Key)
-        private static final String DESCRIPTION = "description";    //Column II
-        private static final String IMAGE= "ImageByte";//Column III
-        private static final String THEME= "Theme"; //Column IIII
-        private static final String DATE= "DatePosted"; //Column IIIII
-        private static final String LIKE= "Likes";    // Column IIIIII
-        //Command to create the Image table
-        private static final String CREATE_IMAGE_TABLE =
-                "CREATE TABLE "+IMAGE_TABLE
+        public static final String IMAGE_TABLE = "Images";   // Table Name
+        public static final String IMAGE_ID = "imageID";     // Column I (Primary Key)
+        public static final String DESCRIPTION = "description";    //Column II
+        public static final String IMAGE = "ImageByte";//Column III
+        public static final String THEME = "Theme"; //Column IIII
+        public static final String LIKE = "Likes";    // Column IIIII
+        // Command to create the Image table
+        public static final String CREATE_IMAGE_TABLE =
+                "CREATE TABLE IF NOT EXISTS " + IMAGE_TABLE
                         + " ("
-                        + IMAGE_ID+" INTEGER PRIMARY KEY, "
-                        + IMAGE+"BLOB ,"
-                        + DESCRIPTION+" VARCHAR(255) ,"
-                        + THEME+" VARCHAR(255) ,"
-                        + DATE+" DATETIME DEFAULT (datetime('now','localtime')) ,"
-                        + LIKE+" BIGINT DEFAULT 0 ,"
-                        + NAME+" VARCHAR(255) ,"
-                        + " FOREIGN KEY ("+NAME+") REFERENCES "+MEMBER_TABLE+"("+NAME+")"
+                        + IMAGE_ID + " INTEGER PRIMARY KEY ,"
+                        + IMAGE + " BLOB ,"
+                        + DESCRIPTION + " VARCHAR(255) ,"
+                        + THEME + " VARCHAR(255) ,"
+                        + LIKE + " BIGINT DEFAULT 0 ,"
+                        + NAME + " VARCHAR(255) ,"
+                        + " FOREIGN KEY (" + NAME + ") REFERENCES " + MEMBER_TABLE + "(" + NAME + ")"
                         + ");";
         //Drop table for Image
-        private static final String DROP_IMAGE_TABLE ="DROP TABLE IF EXISTS "+IMAGE_TABLE;
+        public static final String DROP_IMAGE_TABLE = "DROP TABLE IF EXISTS " + IMAGE_TABLE;
 
         //Defining the Like table variables
-        private static final String LIKE_TABLE = "Likes";   // Table Name// Column I (Primary Key)//Column II
-        private static final String STATUS= "Status";    // Column III
+        public static final String LIKE_TABLE = "Likes";   // Table Name// Column I (Primary Key)//Column II
+        public static final String STATUS = "Status";    // Column III
         //Command to create the Like table
-        private static final String CREATE_LIKE_TABLE =
-                "CREATE TABLE " + LIKE_TABLE
-                + " ("
-                + IMAGE_ID +" INTEGER ,"
-                + NAME+" VARCHAR(255) ,"
-                + STATUS+" BOOL DEFAULT 1,"
-                + "PRIMARY KEY"+"("+IMAGE_ID+","+NAME+"), "
-                + "FOREIGN KEY ("+NAME+") REFERENCES "+MEMBER_TABLE+"("+NAME+"), "
-                + "FOREIGN KEY ("+IMAGE_ID+") REFERENCES "+IMAGE_TABLE+"("+IMAGE_ID+")"
-                + ");";
+        public static final String CREATE_LIKE_TABLE =
+                "CREATE TABLE IF NOT EXISTS " + LIKE_TABLE
+                        + " ("
+                        + IMAGE_ID + " INTEGER ,"
+                        + NAME + " VARCHAR(255) ,"
+                        + STATUS + " BOOL DEFAULT 1,"
+                        + "PRIMARY KEY" + "(" + IMAGE_ID + "," + NAME + "), "
+                        + "FOREIGN KEY (" + NAME + ") REFERENCES " + MEMBER_TABLE + "(" + NAME + "), "
+                        + "FOREIGN KEY (" + IMAGE_ID + ") REFERENCES " + IMAGE_TABLE + "(" + IMAGE_ID + ")"
+                        + ");";
         //Drop table for Like
-        private static final String DROP_LIKE_TABLE ="DROP TABLE IF EXISTS "+ LIKE_TABLE;
+        public static final String DROP_LIKE_TABLE = "DROP TABLE IF EXISTS " + LIKE_TABLE;
 
         public myDbHelper(Context context) {
             super(context, DATABASE_NAME, null, DATABASE_Version);
-            this.context=context;
+            this.context = context;
         }
 
 
         //Create the table when the activity is created
         public void onCreate(SQLiteDatabase db) {
-
-            try {
+             /*try {
                 db.execSQL(DROP_MEMBER_TABLE);
                 db.execSQL(DROP_IMAGE_TABLE);
                 db.execSQL(DROP_LIKE_TABLE);
@@ -109,6 +107,7 @@ public class PictureDBAdapter extends AppCompatActivity {
             } catch (Exception e) {
                 Message.message(context,""+e);
             }
+        }*/
         }
 
 
@@ -124,11 +123,24 @@ public class PictureDBAdapter extends AppCompatActivity {
             }*/
         }
     }
+    public void createTable()
+    {
+        SQLiteDatabase db = myhelper.getWritableDatabase();
+        db.execSQL(DROP_MEMBER_TABLE);
+        db.execSQL(DROP_IMAGE_TABLE);
+        db.execSQL(DROP_LIKE_TABLE);
+        db.execSQL(CREATE_MEMBER_TABLE);
+        db.execSQL(CREATE_IMAGE_TABLE);
+        db.execSQL(CREATE_LIKE_TABLE);
+        addMemberData();
+        addImageData();
+        addLikeData();
+    }
+
     public void addMemberData()
     {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("Account");
-        SQLiteDatabase db = myhelper.getWritableDatabase();
         myRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -156,15 +168,16 @@ public class PictureDBAdapter extends AppCompatActivity {
                 ContentValues contentValues = new ContentValues();
                 contentValues.put(myDbHelper.IMAGE_ID, snapshot.child("imageID").getValue(Integer.class));
                 contentValues.put(myDbHelper.NAME, snapshot.child("userName").getValue(String.class));
-                contentValues.put(myDbHelper.IMAGE, snapshot.child("imageByte").getValue(Byte.class));
                 contentValues.put(myDbHelper.LIKE, snapshot.child("likes").getValue(Integer.class));
-                contentValues.put(myDbHelper.DATE, snapshot.child("datePosted").getValue(String.class));
+                contentValues.put(myDbHelper.DESCRIPTION,snapshot.child("description").getValue(String.class));
+                contentValues.put(myDbHelper.THEME,snapshot.child("theme").getValue(String.class));
+                contentValues.put(myDbHelper.IMAGE, (snapshot.child("imageByte").getValue(String.class)).getBytes());
                 long string = db.insert(myDbHelper.IMAGE_TABLE, null, contentValues);
             }
         }
-        @Override
-        public void onCancelled(DatabaseError databaseError) {
-        }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
     });
     }
     public void addLikeData()
@@ -188,12 +201,14 @@ public class PictureDBAdapter extends AppCompatActivity {
             }
         });
     }
+
     //used to get all the image records from the Image table and convert them to image objects in a list
     public List<Image> getAllImageData()
     {
         List<Image> imageList= new ArrayList<>();
         SQLiteDatabase db = myhelper.getWritableDatabase();
-        Cursor c = db.rawQuery("SELECT * FROM Images WHERE MemberID = ? AND THEME=? AND DatePosted BETWEEN DATE('now', '-7 days')  AND Date('now')",new String[] {"1","Cats"});
+        Cursor c = db.rawQuery("SELECT * FROM Images",null);
+        c.moveToFirst();
         while (c.moveToNext())
         {
             int iid= c.getInt(c.getColumnIndex(myhelper.IMAGE_ID));
@@ -201,17 +216,8 @@ public class PictureDBAdapter extends AppCompatActivity {
             byte[] img=c.getBlob(c.getColumnIndex(myhelper.IMAGE));
             String desc =c.getString(c.getColumnIndex(myhelper.DESCRIPTION));
             int like=c.getInt(c.getColumnIndex(myhelper.LIKE));
-            DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            try
-            {
-                Date date = format.parse(c.getString(c.getColumnIndex(myhelper.DATE)).toString());
-                Image image = new Image(iid, mid, img, desc, like, date);
-                imageList.add(image);
-            }
-            catch(ParseException e)
-            {
-                Toast.makeText(this,e.getMessage(),Toast.LENGTH_SHORT);
-            }
+            Image image = new Image(iid, mid, img, desc, like);
+            imageList.add(image);
         }
         return  imageList;
     }
@@ -219,10 +225,10 @@ public class PictureDBAdapter extends AppCompatActivity {
     public int checkLikeStatus(int imageid, String membername)
     {
         SQLiteDatabase db = myhelper.getWritableDatabase();
-        Cursor c = db.rawQuery("SELECT EXISTS (SELECT * FROM Likes WHERE imageID = ? AND memberID = ?)", new String[]{Integer.toString(imageid) , membername});
+        Cursor c = db.rawQuery("SELECT EXISTS (SELECT * FROM Likes WHERE imageID = ? AND username = ?)", new String[]{Integer.toString(imageid) , membername});
         if(c.getInt(0)==1)
         {
-            Cursor cs = db.rawQuery("SELECT Status FROM Likes WHERE imageID = ? AND memberID = ?", new String[]{Integer.toString(imageid) , membername});
+            Cursor cs = db.rawQuery("SELECT Status FROM Likes WHERE imageID = ? AND username = ?", new String[]{Integer.toString(imageid) , membername});
             if(cs.getInt(0)==1)
             {
                 return 1;
@@ -242,7 +248,7 @@ public class PictureDBAdapter extends AppCompatActivity {
     {
         SQLiteDatabase db = myhelper.getWritableDatabase();
         //Retrieve the previous likes
-        Cursor c = db.rawQuery("SELECT Likes, MemberID, ImageID FROM Images Where imageID = ?", new String[]{id});
+        Cursor c = db.rawQuery("SELECT Likes, username, ImageID FROM Images Where imageID = ?", new String[]{id});
         Integer oldLikes=c.getInt(c.getColumnIndex(myhelper.LIKE));
         Integer imageID=c.getInt(c.getColumnIndex(myhelper.IMAGE_ID));
         Integer newLikes=oldLikes+1;
@@ -272,7 +278,7 @@ public class PictureDBAdapter extends AppCompatActivity {
     {
         SQLiteDatabase db = myhelper.getWritableDatabase();
         //Retrieve the previous likes
-        Cursor c = db.rawQuery("SELECT Likes, MemberID, ImageID FROM Images Where imageID = ?", new String[]{id});
+        Cursor c = db.rawQuery("SELECT Likes, username, ImageID FROM Images Where imageID = ?", new String[]{id});
         Integer oldLikes=c.getInt(c.getColumnIndex(myhelper.LIKE));
         Integer memberID=c.getInt(c.getColumnIndex(myhelper.NAME));
         Integer imageID=c.getInt(c.getColumnIndex(myhelper.IMAGE_ID));
